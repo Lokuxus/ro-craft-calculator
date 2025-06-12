e975bfacf3da2eab91b17b4d2c7340e2eb9821c5
Hélio Jacinto <lokuxus@hotmail.com>
2025-06-12 19:55:10 -0300

    Conversão para TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-
+import {Card} from 'primereact/card'
+import {Dropdown} from 'primereact/dropdown'
+import {InputNumber} from 'primereact/inputnumber'
 import './App.css'
 
 function App() {
@@ -15,68 +17,82 @@ function App() {
     return (
         <>
 
-            <div className="card">
-                <form>
-                    <div>
-                        <label htmlFor="researchPotLevel">Research Pot Level: </label>
-                        <select id="researchPotLevel" value={researchPotLevel}
-                                onChange={(e) => setResearchPotLevel(Number(e.target.value))}>
-                            {[...Array(11)].map((_, i) => (
-                                <option key={i} value={i}>{i}</option>
-                            ))}
-                        </select>
+            <Card className="form-component">
+                <div className="grid align-self-center">
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="researchPotLevel" className="w-12rem">Research Pot Level</label>
+                        <Dropdown id="researchPotLevel"
+                                value={researchPotLevel}
+                                onChange={(e) => setResearchPotLevel(e.value)}
+                                options={[...Array(11)].map((_, i) => ({label: i.toString(), value: i}))}
+                                className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="preparePotLevel">Prepare Pot Level: </label>
-                        <select id="preparePotLevel" value={preparePotLevel}
-                                onChange={(e) => setPreparePotLevel(Number(e.target.value))}>
-                            {[...Array(11)].map((_, i) => (
-                                <option key={i} value={i}>{i}</option>
-                            ))}
-                        </select>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="preparePotLevel" className="w-12rem">Prepare Pot Level</label>
+                        <Dropdown id="preparePotLevel"
+                                value={preparePotLevel}
+                                onChange={(e) => setPreparePotLevel(e.value)}
+                                options={[...Array(11)].map((_, i) => ({label: i.toString(), value: i}))}
+                                className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="humunculusBlessLevel">Humunculus Change of Plans: </label>
-                        <select id="humunculusBlessLevel" value={humunculusChangeofPlans}
-                                onChange={(e) => setHumunculusChangeofPlans(Number(e.target.value))}>
-                            {[...Array(6)].map((_, i) => (
-                                <option key={i} value={i}>{i}</option>
-                            ))}
-                        </select>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="humunculusBlessLevel" className="w-12rem">Humunculus Change of Plans</label>
+                        <Dropdown id="humunculusBlessLevel"
+                                value={humunculusChangeofPlans}
+                                onChange={(e) => setHumunculusChangeofPlans(e.value)}
+                                options={[...Array(6)].map((_, i) => ({label: i.toString(), value: i}))}
+                                className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="LevelJob">Level Job: </label>
-                        <select id="LevelJob" value={LevelJob} onChange={(e) => setLevelJob(Number(e.target.value))}>
-                            {[...Array(71)].map((_, i) => (
-                                <option key={i} value={i}>{i}</option>
-                            ))}
-                        </select>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="LevelJob" className="w-12rem">Level Job</label>
+                        <Dropdown id="LevelJob"
+                                value={LevelJob}
+                                onChange={(e) => setLevelJob(e.value)}
+                                options={[...Array(70)].map((_, i) => ({label: (i+1).toString(), value: i+1}))}
+                                className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="Dificuldade">Dificuldade: </label>
-                        <select id="Dificuldade" value={Dificuldade}
-                                onChange={(e) => setDificuldade(Number(e.target.value))}>
-                            {[...dificuldades].map((_, i) => (
-                                <option key={i} value={_}>{_}%</option>
-                            ))}
-                        </select>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="Dificuldade" className="w-12rem">Dificuldade</label>
+                        <Dropdown id="Dificuldade"
+                                value={Dificuldade}
+                                onChange={(e) => setDificuldade(e.value)}
+                                options={dificuldades.map(value => ({label: `${value}%`, value}))}
+                                className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="INT">INT: </label>
-                        <input type="number" id="INT" value={INT} onChange={(e) => setINT(Number(e.target.value))}/>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="INT" className="w-12rem">INT</label>
+                        <InputNumber id="INT"
+                                   value={INT}
+                                   onValueChange={(e) => setINT(e.value ?? 0)}
+                                   className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="Dex">DEX: </label>
-                        <input type="number" id="Dex" value={Dex} onChange={(e) => setDex(Number(e.target.value))}/>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="Dex" className="w-12rem">DEX</label>
+                        <InputNumber id="Dex"
+                                   value={Dex}
+                                   onValueChange={(e) => setDex(e.value ?? 0)}
+                                   className="w-5rem"
+                        />
                     </div>
-                    <div>
-                        <label htmlFor="SOR">SOR: </label>
-                        <input type="number" id="SOR" value={SOR} onChange={(e) => setSOR(Number(e.target.value))}/>
+                    <div className="col-12 flex align-items-center gap-2">
+                        <label htmlFor="SOR" className="w-12rem">SOR</label>
+                        <InputNumber id="SOR"
+                                   value={SOR}
+                                   onValueChange={(e) => setSOR(e.value ?? 0)}
+                                   className="w-5rem"
+                        />
                     </div>
-                </form>
-                <p>Success Rate:
-                    [{((preparePotLevel * 3) + researchPotLevel + humunculusChangeofPlans) + (LevelJob * 0.2) + (Dex * 0.1) + (SOR * 0.1) + (INT * 0.05) + (Dificuldade)}]%</p>
-            </div>
+                </div>
+                <div className="text-center font-bold col-12 flex align-items-center">
+                    Success Rate: {((preparePotLevel * 3) + researchPotLevel + humunculusChangeofPlans) + (LevelJob * 0.2) + (Dex * 0.1) + (SOR * 0.1) + (INT * 0.05) + (Dificuldade)}%
+                </div>
+            </Card>
 
         </>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {PrimeReactProvider} from "primereact/api";
+import 'primeicons/primeicons.css';
+import {PrimeReactProvider} from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <PrimeReactProvider>
-          <App/>
-      </PrimeReactProvider>
-  </StrictMode>,
+    <StrictMode>
+        <PrimeReactProvider>
+            <App/>
+        </PrimeReactProvider>
+    </StrictMode>,
 )
